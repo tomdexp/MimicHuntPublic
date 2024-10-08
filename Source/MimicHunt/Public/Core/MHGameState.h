41fb653edfd62f92fce680a32afc3f3f5f7589a9
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-10-08 09:52:51 +0200

    [feat] Add MHGameState and BP_MHGameState and set it as default GameState

---
+++ Source/MimicHunt/Public/Core/MHGameState.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "MHGameState.generated.h"
+
+UCLASS()
+class MIMICHUNT_API AMHGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
