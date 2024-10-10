485ed9de4802818256dbcd409cb07812998e0bbe
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-10-10 16:20:21 +0200

    [fix] Disable Unity Builds

--- Source/MimicHunt.Target.cs
+++ Source/MimicHunt.Target.cs
@@ -10,6 +10,7 @@ public class MimicHuntTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		bUseUnityBuild = false;
 		ExtraModuleNames.Add("MimicHunt");
 	}
 }
--- Source/MimicHuntEditor.Target.cs
+++ Source/MimicHuntEditor.Target.cs
@@ -10,6 +10,7 @@ public class MimicHuntEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		bUseUnityBuild = false;
 		ExtraModuleNames.Add("MimicHunt");
 	}
 }
