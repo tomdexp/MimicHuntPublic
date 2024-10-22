0deb640487d92d30c619c3aaa9e7f8e1b844e41a
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-10-22 15:05:02 +0200

    [feat] Add AdvancedEditMode feature to DATA_Player

--- Source/MimicHunt/Public/Data/MHPlayerData.h
+++ Source/MimicHunt/Public/Data/MHPlayerData.h
@@ -16,47 +16,52 @@ class MIMICHUNT_API UMHPlayerData : public UDataAsset
 {
 	GENERATED_BODY()
 public:
-	/** INPUTS */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SETTINGS")
+	bool bAdvancedEditMode = false;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputMappingContext> InputMappingContext;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionMove;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionLook;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionJump;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionCrouch;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionCrouchToggle;
 	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionSprint;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionSprintToggle;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionPrimaryAction;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionSecondaryAction;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "INPUT", meta = (EditCondition = "bAdvancedEditMode", EditConditionHides = true))
 	TObjectPtr<UInputAction> InputActionInteract;
 
-	/** MOVEMENT */
+
+	/*In cm/s*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MOVEMENT")
 	float WalkSpeed = 200.0f;
 
+	/*In cm/s*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MOVEMENT")
 	float SprintSpeed = 400.0f;
 
+	/*In cm/s*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MOVEMENT")
 	float CrouchSpeed = 100.0f;
 
